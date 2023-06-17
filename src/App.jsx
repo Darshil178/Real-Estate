e0c3412e89920f8b972a0427e961bb73abe90285
Darshil178 <120172473+Darshil178@users.noreply.github.com>
2023-06-17 17:46:04 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-
+import Home from './pages/Home';
+import './App.css'
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
 function App() {
   return (
-    <span>subscribe Zainkeepscode</span>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}>
+          </Route>
+        </Routes>
+      </Router>
+      
+    </div>
   );
 }
 
